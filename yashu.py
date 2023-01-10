d6d2bc75b8336da70a4bbe26e3e867dd88ec8c02
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-10 10:23:02 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,8 +1,10 @@
 from pyrogram import Client, filters, idle
 from config import *
+from pyrogram.types import InlineQueryResultArticle as IQRA, InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM, InputMessageContent as IMC, InputTextMessageContent as ITMC
 
 yashu = Client("WHISPER-BOT", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+ALPHA = {}
 
 TXT = "Hello {} ! I'm {}, I can help you to send whispers in various modes !\n\Hit /help to know more !"
 
@@ -18,3 +20,42 @@ async def help(_, m):
     un = (await _.get_me()).username
     await m.reply(HLP.format(un, un))
 
+res = [IQRA(title="Whisper", input_message_content=IMC(ITMC("[USERNAME | ID] [WHISPER]")))]
+res1 = [IQRA(title="Whisper", input_message_content=IMC(ITMC("Invalid Username or Id !")))]
+
+@yashu.on_inline_query()
+async def inline(_, i):
+    global ALPHA
+    txt = i.query.lower()
+    if not len(txt.split(None, 1)) == 2:
+        await _.answer_inline_query(i.id, result=res, cache_time=0)
+    try:
+        id = int(txt[0])
+    except:
+        try:
+            id = (await _.get_users(txt[0])).id
+        except:
+            await _.answer_inline_query(i.id, result=res1, cache_time=0)
+    Na = (await _.get_users(id)).first_name
+    whisp = txt.split(None, 1)[1]
+    WTXT = "A whisper has been sent to {}.\n\nOnly he / she can open it."
+    SHOW = IKM([[IKB("Whisper ☁️", callback_data=f"{i.from_user.id}_{id}")]])
+    res2 = [IQRA(title="Whisper", input_message_content=IMC(ITMC(WTXT.format(Na))), reply_markup=SHOW)]
+    ALPHA[[id, i.from_user.id]] = whisp
+
+@yashu.on_callback_query()
+async def cbq(_, q):
+    id = q.from_user.id
+    mid = q.message.from_user.id
+    if q.data != f"{mid}_{id}":
+        return await q.answer("This is not for you baka !", show_alert=True)
+    try:
+        msg = ALPHA[[id, mid]]
+    except:
+        msg = "Whisper has been deleted from Database !"
+    await q.answer(msg, show_alert=True)
+
+yashu.start()
+print("Started !")
+idle()
+    

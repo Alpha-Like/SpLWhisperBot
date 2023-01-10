69ec9db62f17116c209a2c3a9463bb8d74468fea
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-10 10:25:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from os import getenv as e
 API_ID = e("API_ID", 13691707)
 API_HASH = e("API_HASH", "2a31b117896c5c7da27c74025aa602b8")
 
-BOT_TOKEN = e("BOT_TOKEN", "")
+BOT_TOKEN = e("BOT_TOKEN", "5953961340:AAFiBeDXt7Yb10ShbV2O5z2oCzVTgjEK43g")

f5e6a2f34fc83e57b6af8da98f5fe0227db77f24
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-11 11:06:26 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -42,7 +42,7 @@ async def inline(_, i):
     SHOW = IKM([[IKB("Whisper ☁️", callback_data=f"{i.from_user.id}_{id}")]])
     res2 = [IQRA(title="Whisper", description=f"Send a whisper to {Na} !", input_message_content=ITMC(WTXT.format(Na)), reply_markup=SHOW)]
     await _.answer_inline_query(i.id, results=res2, cache_time=0)
-    ALPHA[i.from_user.id][tar] = whisp
+    ALPHA[str(i.from_user.id)][str(tar)] = whisp
 
 @yashu.on_callback_query()
 async def cbq(_, q):
@@ -53,7 +53,7 @@ async def cbq(_, q):
         if q.data != f"{mid}_{id}":
             return await q.answer("This is not for you baka !", show_alert=True)
         try:
-            msg = ALPHA[mid][id] 
+            msg = ALPHA[str(mid)][str(id)] 
         except:
             msg = "Whisper has been deleted from Database !"
         await q.answer(msg, show_alert=True)

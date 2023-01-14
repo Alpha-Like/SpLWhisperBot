5583fb2279464fb43f6eab6ca3132733db2a6e70
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-14 19:51:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 from os import getenv as e
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
 API_ID = e("API_ID", 13691707)
 API_HASH = e("API_HASH", "2a31b117896c5c7da27c74025aa602b8")
 
 BOT_TOKEN = e("BOT_TOKEN", None)
+
+mongo = MongoClient("mongodb+srv://Cyberpunk:alpha@cluster0.mxvicxt.mongodb.net/?retryWrites=true&w=majority")
+db = mongo.CRED
+
+async def verify(txt):
+    x = await db.credits.find_one({"txt": txt})
+    if not x:
+        return False
+    return True
